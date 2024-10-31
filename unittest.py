18d14f966a825be2c246bee4059645de643792d4
XYEPLET2006 <mihailsamburakov@gmail.com>
2024-10-31 13:26:48 +0300

    Added unittest and CI/CD

---
+++ unittest.py
@@ -0,0 +1,13 @@
+import unittest
+
+def perimeter(a, b):
+    return (a + b) * 2
+
+class RectangleTestCase(unittest.TestCase):
+    def test_rectangle_plus(self):
+        res = perimeter(10, 20)
+        self.assertEqual(res, 60)
+
+    def test_square_plus(self):
+        res = perimeter(10, 10)
+        self.assertEqual(res, 40)
