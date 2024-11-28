0e7b97c32c16ce1445d735187df7cd7cdd4aec9b
XYEPLET2006 <109779992+Reshirampik@users.noreply.github.com>
2024-11-28 11:57:26 +0300

    Added more unittest

--- test.py
+++ test.py
@@ -11,3 +11,10 @@ class RectangleTestCase(unittest.TestCase):
     def test_square_plus(self):
         res = perimeter(10, 10)
         self.assertEqual(res, 40)
+    def test_rectangle_plus1(self):
+        res = perimeter(10, 20)
+        self.assertEqual(res, 60)
+
+    def test_square_plus1(self):
+        res = perimeter(10, 10)
+        self.assertEqual(res, 40)
